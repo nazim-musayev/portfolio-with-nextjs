f515c71cbc51b67b85e41eaff5f08113e5feb60c
nazim-musayev <nazim.musa.555@gmail.com>
2023-04-10 18:21:42 +0400

    adding updates

--- src/components/home/Content.tsx
+++ src/components/home/Content.tsx
@@ -5,11 +5,11 @@ const Content = () => {
     <article>
       <p className="mb-5 text-justify">
         This is my portfolio website, where you can find my projects, contact information
-        and also a little bit personal information about me. <br /> 
+        and also a little bit personal information about me. <br />
         <br />
-        As a React developer and coding enthusiast I am passionate about 
+        As a React developer and coding enthusiast I am passionate about
         learning new technologies and building projects. <br />
-        Also I am really enjoying that trying different solutions for the problems which 
+        Also I am really enjoying that trying different solutions for the problems which
         I am facing in development process.
       </p>
       <ResumeButton />
